d7d9cd6e59505e4dc2960d3d47c0ff8897aee374
Dulan Dias <dulan@echonlabs.com>
2024-08-04 11:55:24 +0100

    partial revert

--- R/calculateExpectedEventTimes.R
+++ R/calculateExpectedEventTimes.R
@@ -34,14 +34,7 @@
 #'
 #' @export
 calculateExpectedEventTimes <- function(model_object, new_observations, n_phases, upper_time = 10000, strata_by, file_path = NULL) {
-  # Load necessary libraries
-  if (!requireNamespace("future", quietly = TRUE)) {
-    stop("The 'future' package is required but not installed.")
-  }
-  if (!requireNamespace("future.apply", quietly = TRUE)) {
-    stop("The 'future.apply' package is required but not installed.")
-  }
-  future::plan(future::multisession, workers = 4)  # Set up parallel plan
+  library(future.apply)
 
   # Convert new_observations to a data frame if it's not already
   new_observations <- as.data.frame(new_observations)
@@ -58,7 +51,7 @@ calculateExpectedEventTimes <- function(model_object, new_observations, n_phases
     tryCatch({
       calculateExpectedEventTime(model_object, row, n_phases, upper_time, strata_by)
     }, error = function(e) {
-      return(NA)  # Return NA on error
+      return(NA)  # Return NA or any default value on error
     })
   }
 
