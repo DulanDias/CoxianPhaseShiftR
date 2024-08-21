a2614a7b601416afaad352e180191c5dcdddfedb
Dulan Dias <dulan@echonlabs.com>
2024-08-21 11:22:38 +0100

    updated documentation

--- R/saveModelToFile.R
+++ R/saveModelToFile.R
@@ -1,21 +1,20 @@
-#' Load Model from File
+#' Save Model to File
 #'
-#' This function loads a model object from a specified file path. It is designed to work with models
-#' that have been saved using `saveRDS()`. This is useful for scenarios where you need to persist
-#' model objects on disk for later use, such as for prediction or further analysis.
+#' This function saves a model object to a specified file path using `saveRDS()`. This is useful for
+#' scenarios where you need to persist model objects on disk for later use, such as for prediction or further analysis.
 #'
-#' @param filepath A string specifying the path to the file where the model object is saved. The file
-#' should have been created using `saveRDS()`.
+#' @param model The model object to be saved.
+#' @param filepath A string specifying the path to the file where the model object should be saved. The file
+#' will be created or overwritten using `saveRDS()`.
 #'
-#' @return The model object loaded from the specified file. If an error occurs during loading, `NULL`
-#' is returned and an error message is printed to the console.
+#' @return NULL. The function is called for its side effects (saving the model to disk). If an error occurs during saving, an error message is printed to the console.
 #'
 #' @examples
 #' \dontrun{
-#'   # Assuming you have previously saved a model to "model.rds"
+#'   # Assuming you have a model object `my_model`
 #'   model_path <- "path/to/your_saved_model.rds"
-#'   loaded_model <- loadModelFromFile(model_path)
-#'   # Now `loaded_model` can be used for predictions or further analysis
+#'   saveModelToFile(my_model, model_path)
+#'   # The model is now saved to the specified file path
 #' }
 #'
 #' @export
